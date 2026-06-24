8e7d6ae8ecde0677d7912a4a3d172dfd8481fc42
Mit Kalathiya <mitkalathiya2104@gmail.com>
2026-06-24 19:36:37 +0530

    TeraBox link updated and Google Analytics script added.

--- videos.js
+++ videos.js
@@ -8,7 +8,7 @@ const videoDatabase = [
     id: 1,
     thumbnail: "images/1.jpg",
     title: "PAYAL GAMING VIRAL PART 2 😁",
-    videoUrl: "https://terasharefile.com/s/1gOUVtHX0IsFJzw5V8nVAYQ",
+    videoUrl: "https://1024terabox.com/s/1SA4X66Gd1p94QpW6yP74Kw",
     views: 184000,
     duration: "14:22"
   },
@@ -16,7 +16,7 @@ const videoDatabase = [
     id: 2,
     thumbnail: "images/2.jpg",
     title: "🔥 🥵 INSTA 19 MIN 34 SECOND VIRAL VIDEO WATCH FULL",
-    videoUrl: "https://terasharefile.com/s/139XZYdWNOyfSGAYgh89iyA",
+    videoUrl: "https://1024terabox.com/s/1Yty_dJDkt3mnGpyyoOPkJg",
     views: 135000,
     duration: "18:45"
   },
@@ -24,7 +24,7 @@ const videoDatabase = [
     id: 3,
     thumbnail: "images/3.jpg",
     title: "Dani Daniels Pronfidelty Sex Movies",
-    videoUrl: "https://terasharefile.com/s/1FGZ0zjba2RBobJViEYWMoA",
+    videoUrl: "https://1024terabox.com/s/1Ix7VJG5bbMK7CB0dlmwZAA",
     views: 96000,
     duration: "09:15"
   },
@@ -32,7 +32,7 @@ const videoDatabase = [
     id: 4,
     thumbnail: "images/4.jpg",
     title: "Mothers & Stepsons Vol. 8 Porn Movies",
-    videoUrl: "https://terasharefile.com/s/1-OACqFxgXeOVj2ys_JPfhw",
+    videoUrl: "https://1024terabox.com/s/128cP9HIg76SXz6Rel-Px_Q",
     views: 520000,
     duration: "24:00"
   },
@@ -40,7 +40,7 @@ const videoDatabase = [
     id: 5,
     thumbnail: "images/5.jpg",
     title: "Diesel Dongs Vol. 41 (2024) PORN MOVIES",
-    videoUrl: "https://terasharefile.com/s/1tq2_kaIp-5Yufgo08AUhWQ",
+    videoUrl: "https://1024terabox.com/s/1pcEWHEfBGrYnUZC3dUiafA",
     views: 310000,
     duration: "12:30"
   },
@@ -48,7 +48,7 @@ const videoDatabase = [
     id: 6,
     thumbnail: "images/6.jpg",
     title: "INDIAN COLLEGE GIRL XXX",
-    videoUrl: "https://terasharefile.com/s/1hP1WPP6YA2y2ZfGyBW1pRw",
+    videoUrl: "https://1024terabox.com/s/160ORynU7C3oV0fmUXJm3kg",
     views: 145000,
     duration: "22:10"
   },
@@ -56,7 +56,7 @@ const videoDatabase = [
     id: 7,
     thumbnail: "images/7.jpeg",
     title: "SAPNA SHAH VIRAL VIDEO",
-    videoUrl: "https://terasharefile.com/s/1FrKWW8CfrjwWOQP_Pnjizw",
+    videoUrl: "https://1024terabox.com/s/1pIRzLsa93zqqfJn7sQjBPg",
     views: 420000,
     duration: "15:40"
   },
@@ -64,7 +64,7 @@ const videoDatabase = [
     id: 8,
     thumbnail: "images/8.webp",
     title: "🔥 PAKISTANI VIRAL VIDEO MARRY 😄 🤣",
-    videoUrl: "https://terasharefile.com/s/1AAXrQG_oKRIiguDtIeiTqA",
+    videoUrl: "https://1024terabox.com/s/1ma35BVEE0nLtDgIxGuJBVQ",
     views: 88000,
     duration: "35:20"
   },
@@ -72,7 +72,7 @@ const videoDatabase = [
     id: 9,
     thumbnail: "images/9.jpg",
     title: "SAB MAL MUH MA NIKAL DIYA",
-    videoUrl: "https://terasharefile.com/s/174hH8r2RUwibD8ChJ-qArw",
+    videoUrl: "https://1024terabox.com/s/1o7J35-FHkCSEvuPRFsrQZA",
     views: 110000,
     duration: "11:55"
   },
@@ -80,7 +80,7 @@ const videoDatabase = [
     id: 10,
     thumbnail: "images/10.jpg",
     title: "INDIAN DESI VIRAL MMS",
-    videoUrl: "https://terasharefile.com/s/17RB-t3rYuonTyieOEpR3yA",
+    videoUrl: "https://1024terabox.com/s/1LO8r472LM8COoTxwHSfcZQ",
     views: 75000,
     duration: "13:50"
   },
@@ -88,7 +88,7 @@ const videoDatabase = [
     id: 11,
     thumbnail: "images/11.jpg",
     title: "ANAM INDIAN GIRL HUG IN MOUTH XXX",
-    videoUrl: "https://terasharefile.com/s/1IJvYfY3tpUgegxzlEeL2hg",
+    videoUrl: "https://1024terabox.com/s/1U3QolUrr_pR3VnyiEENmCQ",
     views: 640000,
     duration: "20:15"
   },
@@ -96,7 +96,7 @@ const videoDatabase = [
     id: 12,
     thumbnail: "images/12.jpg",
     title: "Addicted To Bush 3 (2024) Adult Movies",
-    videoUrl: "https://terasharefile.com/s/1V-8tGbbMd1BFZrOPVPWufw",
+    videoUrl: "https://1024terabox.com/s/1ZgyGswgA-qKigUllG1Ny6g",
     views: 130000,
     duration: "19:40"
   }
