9760ea4027feeac4916c9c42bc066848c42f3ab5
Mit Kalathiya <mitkalathiya2104@gmail.com>
2026-06-25 20:35:47 +0530

    new data inserted

--- videos.js
+++ videos.js
@@ -1,6 +1,6 @@
 /**
  * VideoHub - Minimal Global Video Database
- * Shared between index.html and video.html
+ * Shared by the homepage and direct video redirect flow
  */
 
 const videoDatabase = [
@@ -8,7 +8,7 @@ const videoDatabase = [
     id: 1,
     thumbnail: "images/1.jpg",
     title: "PAYAL GAMING VIRAL PART 2 😁",
-    videoUrl: "https://1024terabox.com/s/1SA4X66Gd1p94QpW6yP74Kw",
+    videoUrl: "https://1024terabox.com/s/1J2lPAg-tAsbgOu7g1J7jMA",
     views: 184000,
     duration: "14:22"
   },
@@ -99,7 +99,311 @@ const videoDatabase = [
     videoUrl: "https://1024terabox.com/s/1ZgyGswgA-qKigUllG1Ny6g",
     views: 130000,
     duration: "19:40"
-  }
+  },
+  {
+    id: 13,
+    thumbnail: "images/13.jpg",
+    title: "Too Big for Tight Holes (2024) Sex Movies",
+    videoUrl: "https://1024terabox.com/s/1rOOqHLM7mpkl4xGZtWgv-w",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 14,
+    thumbnail: "images/14.jpg",
+    title: "18 YEAR OLD OUTDOOR BLOWJOB HANDJOB XXX",
+    videoUrl: "https://1024terabox.com/s/1yF8t51L7j3T48timaeaRTg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 15,
+    thumbnail: "images/15.jpg",
+    title: "GLASSES GIRL VIRAL MMS",
+    videoUrl: "https://1024terabox.com/s/1KSFDncHckv26Jtry04gttA",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 16,
+    thumbnail: "images/16.jpg",
+    title: "PAKISTANI WIFE SUCKING FEET AND ANAL",
+    videoUrl: "https://1024terabox.com/s/1hjto-o9vvMKD0SqNhbOIzw",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 17,
+    thumbnail: "images/17.jpg",
+    title: "INDIAN COUPLE NA SHADI KI PHLAI RAT KI VIDEO UPLOAD KAR DI",
+    videoUrl: "https://1024terabox.com/s/1tpmwCDKbmALPKAY4fLJwbg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 18,
+    thumbnail: "images/18.jpg",
+    title: "BAGWAN ASI GF HAR KISI KO MILAY",
+    videoUrl: "https://1024terabox.com/s/1Diso8i1IdDmRGV4tpO1PVw",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 19,
+    thumbnail: "images/19.jpg",
+    title: "🇵🇰 PAKISTANI JANNAT MIRZA VIRAL MMS",
+    videoUrl: "https://1024terabox.com/s/1e270RK_tXyicU9D8nqAKBQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 20,
+    thumbnail: "images/20.jpg",
+    title: "Indian Girl Blowjob Mouth Hug Xxx Videos Sex",
+    videoUrl: "https://1024terabox.com/s/1uQez1xrzNZ1dtPawDJanSg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 21,
+    thumbnail: "images/21.jpg",
+    title: "YE PUBLIC AREA MA KAYA CHL RHA HA",
+    videoUrl: "https://1024terabox.com/s/1zbWjdKzchmPvCLsbnPytKA",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 22,
+    thumbnail: "images/22.jpg",
+    title: "INDIAN HOSTEL VIRAL 3 GIRLS VIDEO",
+    videoUrl: "https://1024terabox.com/s/1v_Hy4a4O_LR5f34x1A7diQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 23,
+    thumbnail: "images/23.jpeg",
+    title: "INDIAN UNIVERSITY GIRLS VIRAL VIDEOS",
+    videoUrl: "https://1024terabox.com/s/10cBMSf2o4mGg1PFpbCBm6Q",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 24,
+    thumbnail: "images/24.jpg",
+    title: "Indian Desi Porn Blowjob Fucking Videos",
+    videoUrl: "https://1024terabox.com/s/1WNKRgQfZlw9gZQ9YTNsnog",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 25,
+    thumbnail: "images/25.jpg",
+    title: "INDIAN GIRL HARD FUCK HAND BAND KAR 🔥",
+    videoUrl: "https://1024terabox.com/s/1p4Yp40RXXlql_tPYonzeKQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 26,
+    thumbnail: "images/26.jpg",
+    title: "Pakistani Girl Blowjob In Car Xxx Video",
+    videoUrl: "https://1024terabox.com/s/11zsADzTS-seTR6hod9bG8Q",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 27,
+    thumbnail: "images/27.jpg",
+    title: "Fingering Masturbation Xxxx Videos",
+    videoUrl: "https://1024terabox.com/s/1h9cYHjFHJA1T8cF-VrWiWQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 28,
+    thumbnail: "images/28.jpg",
+    title: "🔥PAYAL GAMING VIRAL LEAK VIDEO",
+    videoUrl: "https://1024terabox.com/s/1DbcYxEx3cIGmzIwG6WAvOQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 29,
+    thumbnail: "images/29.jpg",
+    title: "KULLHAD PIZZA NEW VIRAL VIDEO 2026",
+    videoUrl: "https://1024terabox.com/s/183CvCRwejfDQpvOaKlmiIg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 30,
+    thumbnail: "images/30.jpg",
+    title: "KAYA CHUDAI CHAL RHI HA YR",
+    videoUrl: "https://1024terabox.com/s/1BMDz_SVHts5dhwSi0RxPLw",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 31,
+    thumbnail: "images/31.jpg",
+    title: "INDIAN GIRLS FUCK WITH SLAPPING FACE 🥵 FULL VIDEO",
+    videoUrl: "https://1024terabox.com/s/1JvU2CGY55jEmcF75pArrtA",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 32,
+    thumbnail: "images/32.jpg",
+    title: "🔥 SOFIK SONALI PART 3 LAST WATCH NOW",
+    videoUrl: "https://1024terabox.com/s/1-u2zFc7s6cP9Hcd6PCg1AQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 33,
+    thumbnail: "images/33.jpg",
+    title: "Don’t Break Me Vol. 49 (2024) Adult Movies",
+    videoUrl: "https://1024terabox.com/s/14Za-oQpWbyf-sSbSR7XRtw",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 34,
+    thumbnail: "images/34.jpeg",
+    title: "KAJAL KUMARI VIRAL VIDEO 🔥",
+    videoUrl: "https://1024terabox.com/s/1fU3sq8i-7OpIZU-owZTIOQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 35,
+    thumbnail: "images/35.jpg",
+    title: "BLOWJOB AND FUCK IN CAR",
+    videoUrl: "https://1024terabox.com/s/1RYVwnoHNWdEJRO4nGYg2fQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 36,
+    thumbnail: "images/36.jpg",
+    title: "INDIAN VIRAL GIRL VIDEO 🔥",
+    videoUrl: "https://1024terabox.com/s/1SGKSjEnzt1cXp4w6WO9rfg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 37,
+    thumbnail: "images/37.jpg",
+    title: "🔥 🥵SOFIK SONALI PART 2 FULL VIDEO 55 MIN",
+    videoUrl: "https://1024terabox.com/s/1ylVoynC0qX4UAYQUqnYeJg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 38,
+    thumbnail: "images/38.jpg",
+    title: "PART 1 VIRAL WATCH 🔥 🥵",
+    videoUrl: "https://1024terabox.com/s/1tYSTV_h-oUhcpjrN3iLVAg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 39,
+    thumbnail: "images/39.jpg",
+    title: "Interracial Surrender 17 Porn Movies",
+    videoUrl: "https://1024terabox.com/s/1xedSJSlt1RBq7LxagjzGUg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 40,
+    thumbnail: "images/40.jpg",
+    title: "INDIAN METRO K ANDAR FULL CHUDAI BLOWJOB",
+    videoUrl: "https://1024terabox.com/s/1RrK-9s2Kd1Tb9f-xRWPhKg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 41,
+    thumbnail: "images/41.jpg",
+    title: "🔥 ASHOK KHARAT VIRAL MMS",
+    videoUrl: "https://1024terabox.com/s/1q5LImOT7foxpFyK6517C8A",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 42,
+    thumbnail: "images/42.jpg",
+    title: "18 YEAR BOY BLOWJOB MOM",
+    videoUrl: "https://1024terabox.com/s/1Otc7LGW6rQ2zQpkp9ZYXLQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 43,
+    thumbnail: "images/43.jpg",
+    title: "DEEPAK KALAL NEW FUCK VIRAL VIDEO",
+    videoUrl: "https://1024terabox.com/s/1iH9KB--qUZ8Dq81suH46YQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 44,
+    thumbnail: "images/44.jpg",
+    title: "XXX FUCKING VIDEOS",
+    videoUrl: "https://1024terabox.com/s/1TY4slHAK59X5Ga-SQbZMeQ",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 45,
+    thumbnail: "images/45.jpg",
+    title: "jaisalmer viral Video tau 😄",
+    videoUrl: "https://1024terabox.com/s/1iwiHOG3UJ0cC84XnIjS7DA",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 46,
+    thumbnail: "images/46.jpg",
+    title: "SCHOOL MA YE KON SI PADHI CHAL RHI HA",
+    videoUrl: "https://1024terabox.com/s/1I2xC3qnQqvb9_5XWnqMCtg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 47,
+    thumbnail: "images/47.jpg",
+    title: "FATIMA JATOI PAKISTANI TIKTOKER VIRAL MMS 6 MIN",
+    videoUrl: "https://1024terabox.com/s/14CTP4suJS8XC8MR0W4S_uA",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 48,
+    thumbnail: "images/48.jpg",
+    title: "INDIAN YOUNG COUPLE XXX",
+    videoUrl: "https://1024terabox.com/s/1t_bLubjzOYaNrfjfeFejzg",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 49,
+    thumbnail: "images/49.jpg",
+    title: "🔥HOT INDIAN SCHOOL GIRL FUCK CLASS FELLOW",
+    videoUrl: "https://1024terabox.com/s/1QpDGLyMPFmUCyMMyzvaDwA",
+    views: 130000,
+    duration: "19:40"
+  },
+  {
+    id: 50,
+    thumbnail: "images/50.jpg",
+    title: "PAWG Pleasures 2 (2024) Sex Movies",
+    videoUrl: "https://1024terabox.com/s/1JkQLrlivRR1F5-cnO0g_yg",
+    views: 130000,
+    duration: "19:40"
+  },
 ];
 
 // Attach database to window for global access
